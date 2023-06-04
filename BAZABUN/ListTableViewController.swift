9b3b00604af4bf5c8a9ca1173597b5d41bc1f2db
artemyderik <derry0808@gmail.com>
2023-06-04 13:52:39 +0700

    Progress bold itallic at listVC added.

--- BAZABUN/ListTableViewController.swift
+++ BAZABUN/ListTableViewController.swift
@@ -15,7 +15,7 @@ class ListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = brandColor
-        tableView.rowHeight = 80
+        tableView.rowHeight = 100
         navigationItem.titleView?.backgroundColor = .black
     }
     
@@ -25,6 +25,7 @@ class ListTableViewController: UITableViewController {
        // Food.getTypes().count
     }
     
+     /*
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
         switch food[section].type {
@@ -45,6 +46,7 @@ class ListTableViewController: UITableViewController {
         //return result
         //Food.getTypes()[section]
     }
+      */
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         food[section].products.count
@@ -59,12 +61,13 @@ class ListTableViewController: UITableViewController {
         
        // let foodItem = menu[indexPath.row]
         var content = cell.defaultContentConfiguration()
-        
+
         cell.backgroundColor = brandColor
         
         content.text = foodItem.name
+        
         //content.textProperties.font = UIFont(name: "centurygothic", size: 18) ?? UIFont.systemFont(ofSize: 20)
-        content.textProperties.font = UIFont(name: "Intro", size: 18) ?? UIFont.systemFont(ofSize: 20)
+        content.textProperties.font = UIFont(name: "Intro", size: 21) ?? UIFont.systemFont(ofSize: 20)
         content.textProperties.color = .white
         
         content.image = UIImage(named: foodItem.photo) ?? UIImage(named: "bazabun_logo")
@@ -107,8 +110,8 @@ extension ListTableViewController {
         let foodTypeLabel = UILabel(
             frame: CGRect(
                 x: 15,
-                y: -8,
-                width: 300,
+                y: 0,
+                width: view.frame.width,
                 height: 40
             )
         )
@@ -128,12 +131,19 @@ extension ListTableViewController {
             foodTypeLabel.text = "??"
         }
         
-        foodTypeLabel.font = UIFont(name: "Intro", size: 29)
+        foodTypeLabel.font = UIFont(name: "Progress-BoldItalic", size: 33)
         foodTypeLabel.textColor = lightBrandColor
+        foodTypeLabel.backgroundColor = brandColor
         
         let contentView = UIView()
         contentView.addSubview(foodTypeLabel)
         
         return contentView
     }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        //view.backgroundColor = .black
+    }
+    
+    
 }
