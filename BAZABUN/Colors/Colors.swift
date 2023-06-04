a58a2a64f70078cd75781044c94667a8d24d5c77
artemyderik <derry0808@gmail.com>
2023-06-04 13:13:04 +0700

    Set new assets. Add and delete assets.

--- BAZABUN/Colors/Colors.swift
+++ BAZABUN/Colors/Colors.swift
@@ -12,3 +12,6 @@ let discriptionColor = UIColor(red: 0.61, green: 0.61, blue: 0.61, alpha: 1)
 
 ///Brand light mustard color
 let brandColor = UIColor(red: 0.929, green: 0.67, blue: 0.1725, alpha: 1)
+
+///Brand light color for barely noticeable titles on brand mustard color
+let lightBrandColor = UIColor(red: 0.941, green: 0.729, blue: 0.31, alpha: 1)
